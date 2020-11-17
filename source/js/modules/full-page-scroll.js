b2ef950aae950f3cb861614aff344e887e6f7216
kust1822 <kochurekova@gmail.com>
2020-11-18 00:03:21 +0300

    Добавление побуквенной анимации

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -66,7 +66,7 @@ export default class FullPageScroll {
     } else {
         drove.classList.remove("drove");
     }
-}
+  }
 
   changeVisibilityDisplay() {
     this.screenElements.forEach((screen) => {
